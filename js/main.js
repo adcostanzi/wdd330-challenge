a029d4952622a9ef958973bfc1ec88e417bf5aff
adcostanzi <andres.costanzi@outlook.com>
2023-12-05 23:10:18 -0600

    filter div in header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-import { loadHeaderFooter, searchFunction } from "./utils.mjs";
+import { loadHeaderFooter, searchFunction, filtersFunctionOn } from "./utils.mjs";
 
 async function main() {
     await loadHeaderFooter();
     
     let searchBtn = document.querySelector("#search-btn");
     let searchBar = document.querySelector("#search-bar");
+    let filters = document.querySelector("#filters-btn");
 
     searchBar.addEventListener("keypress", function(e){
       if (e.key === "Enter"){
@@ -12,6 +13,7 @@ async function main() {
       }
     });
     searchBtn.addEventListener("click", searchFunction);
+    filters.addEventListener("click", filtersFunctionOn);
   }
   
   main();
--- js/utils.mjs
+++ js/utils.mjs
@@ -62,3 +62,178 @@ export function convertParameter(searchParameter) {
   // Converts user input " " into +
 return searchParameter.replace(" ", "+");
 }
+
+export function filtersFunctionOn(){
+  const searchByHTML = `
+  <form name="filters">
+  <fieldset class="search-by">
+      <legend>Search by</legend>
+      <label class="filter-radio-option" for="filter">           
+      <input type="radio" value="1" name="search-filter" id="recipe-filter"checked>    
+      Recipe Filters
+       </label>
+       
+      <label class="filter-radio-option">
+      <input type="radio" value="2" name="search-filter" id="ingredients-filter">    
+      Ingredients
+      </label>
+  </fieldset>
+  <fieldset class>
+      <legend>Filters</legend>
+      <div class="filter-options"> </div>
+  </fieldset>
+</form>
+  `;
+  let div = document.querySelector(".header-section");
+  div.insertAdjacentHTML("beforeend", searchByHTML);
+
+  renderCheckedOption();
+  let recipe = document.querySelector("#recipe-filter");
+  let ingredient = document.querySelector("#ingredients-filter");
+  recipe.addEventListener("input", renderCheckedOption);
+  ingredient.addEventListener("input", renderCheckedOption);
+}
+
+function renderCheckedOption(){
+  let recipe = document.querySelector("#recipe-filter");
+  let ingredient = document.querySelector("#ingredients-filter");
+  if (recipe.checked){
+    renderRecipeFilterOptions()
+  } else if (ingredient.checked){
+    renderIngredientsFilterOptions();
+  }
+}
+
+function renderRecipeFilterOptions(){
+
+  const recipeFilterHTML = `
+  <label class="select-filter">
+  Cuisine: 
+  <select name="cuisine" id="cuisine">
+  </select>
+</label>
+<label class="select-filter">
+  Diet: 
+  <select name="diet" id="diet">
+  </select>
+</label>
+<label class="select-filter">
+  Intolerances: 
+  <select name="intolerances" id="intolerances">
+  </select>
+</label>
+<label class="range-filter">
+  Max Calories: <output id="user-calories"></output>
+  <input name="calories" id="calories" type="range" min="0" max="5000" step="10">
+</label>
+  `;
+let div = document.querySelector(".filter-options");
+div.innerHTML = "";
+div.insertAdjacentHTML("beforeend", recipeFilterHTML);
+  
+
+loadOptions();
+
+let showCalories = document.querySelector("#user-calories");
+let userCalories = document.querySelector("#calories");
+showCalories.textContent = userCalories.value;
+
+userCalories.addEventListener("input", (e) => {
+  showCalories.textContent = e.target.value;
+});
+}
+
+
+
+async function loadOptions(){
+  let response = await fetch("/json/options.json");
+  let options;
+  if (response.ok){
+    options = await response.json();
+  }
+  let cuisineSelect = document.querySelector("#cuisine");
+  let dietSelect = document.querySelector("#diet");
+  let intolerancesSelect = document.querySelector("#intolerances");
+
+
+  let cuisineOptions = options.cuisine;
+  let dietOptions = options.diet;
+  let intolerancesOptions = options.intolerances;
+
+  cuisineOptions.forEach(cuisine => {
+    let option = new Option(`${cuisine}`, cuisine);
+    cuisineSelect.append(option);
+  });
+
+  dietOptions.forEach(diet => {
+    let option = new Option(`${diet}`, diet);
+    dietSelect.append(option);
+  });
+
+  intolerancesOptions.forEach(intolerance => {
+    let option = new Option(`${intolerance}`, intolerance);
+    intolerancesSelect.append(option);
+  });
+
+  
+}
+
+function renderIngredientsFilterOptions(){
+
+  const ingredientsFilterHTML = `
+  <label>
+  Add Ingredient:
+  <div class="add-ingredient-container">
+  <input name="ingredient" id="ingredient" type="text" required>
+  <input type="submit" id="add-ingredient" name="add-ingredient" value="Add" class="">
+  </div>
+  </label>
+  <label class="select-filter">Ingredients:</label>
+  <div class="ingredient-list-search">
+  </div>
+  `;
+
+  let div = document.querySelector(".filter-options");
+  div.innerHTML = "";
+  div.insertAdjacentHTML("beforeend", ingredientsFilterHTML);
+
+// Add button functionality
+let addBtn = document.forms["filters"];
+addBtn.addEventListener("submit", (e)=> {
+  e.preventDefault();
+  addIngredientTag();
+});
+
+}
+
+function addIngredientTag(){
+  // Create Tag with user provided name of ingredient and add functionality to be used and removed.
+  let userIngredient = document.querySelector("#ingredient");
+
+  let newIngredient = document.createElement("div");
+  newIngredient.classList.add("tag-search-ingredient");
+  newIngredient.setAttribute("data-name", `${userIngredient.value}`);
+
+  let ingredientName = document.createElement("span");
+  ingredientName.textContent = userIngredient.value;
+
+  let closeBtn = document.createElement("span");
+  closeBtn.classList.add("close-tag");
+  closeBtn.setAttribute("data-name", `${userIngredient.value}`);
+  closeBtn.innerHTML = `X`;
+  closeBtn.addEventListener("click", removeTag);
+
+
+  newIngredient.appendChild(ingredientName);
+  newIngredient.appendChild(closeBtn);
+
+  let divContainer = document.querySelector(".ingredient-list-search");
+  divContainer.appendChild(newIngredient);
+
+  userIngredient.value = "";
+  userIngredient.focus();
+}
+
+function removeTag(){
+  this.parentElement.remove();
+}
